7a253559b8b924dc557a6d6fc03555819ac0ad7c
Assisneto <netodamasceno12@gmail.com>
2019-04-29 22:34:04 -0300

    components roots

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("Hello World");
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+class Button extends Component {
+  render() {
+    return <a href="">Salvar</a>;
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+render(<App />, document.getElementById("App"));
